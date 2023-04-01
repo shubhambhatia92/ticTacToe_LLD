005f11e58cef47b10850d6d68fa122e7280097d7
shubhambhatia92 <shubhambhatia92@gmail.com>
2023-04-01 13:08:39 +0530

    game winning strategy updated

--- src/main/java/org/tictactoe/strategies/gameWinningstrategies/GameWinningStrategy.java
+++ src/main/java/org/tictactoe/strategies/gameWinningstrategies/GameWinningStrategy.java
@@ -8,6 +8,9 @@ public interface GameWinningStrategy {
     boolean checkWinner(Board board,
                         Player lastMovePlayer,
                         Cell moveCell);
+
+    // undo and check draw can be implemented in game class itself but if different winning strategy has there
+    // own way of draw or only then to implement(This is just showcase of design pattern)
     boolean checkDraw(int TotalMoveCount , int dimension);
 
     public boolean doUndo(char symbol,int row , int col);
